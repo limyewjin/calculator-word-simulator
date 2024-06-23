11f9e9579da21a1360e0700507a75d3b92e2eb00
Yew Jin <yewjin@example.com>
2024-06-23 08:44:11 -0700

    relative import

--- src/components/CalculatorWordSimulator.js
+++ src/components/CalculatorWordSimulator.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import { Input } from '../ui/input';
-import { Button } from '../ui/button';
+import { Input } from './ui/Input';
+import { Button } from './ui/Button';
 
 const SevenSegmentDigit = ({ digit, hasDecimal, upsideDown = false }) => {
   const segments = {
